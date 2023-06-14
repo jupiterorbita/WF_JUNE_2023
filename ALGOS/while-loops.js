15d5be21ff4fbbf7bc1104a9a9cf1199d513e6ce
jupiterorbita <38020888+jupiterorbita@users.noreply.github.com>
2023-06-14 11:02:23 -0600

    add algos w1d3

---
+++ ALGOS/while-loops.js
@@ -0,0 +1,38 @@
+// while loops
+
+// 1 - 10
+for(var x = 1; x <= 10; x++) {
+    console.log(x);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// declare a start
+var x = 1;
+
+while(x <= 10) {
+    console.log(x)
+
+    // work towards a break
+    x++;
+}
+
+
+
+
+
